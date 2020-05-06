14bc9d6f201a0cb18deb563bbedbc18fc152f497
russell-ligon <russell.ligon@gmail.com>
2020-05-06 12:49:15 -0400

    Update WekaText_to_List.R

--- WekaText_to_List.R
+++ WekaText_to_List.R
@@ -3,17 +3,25 @@
 
 
 library(recommenderlab)
-
+setwd("/workdir/Ligon/Thermal/8x8thermal/April2020_CoolStuff/TextOut")
 
 TextFiles<-list.files(,pattern=".txt")
-
+# create progress bar
+pb <- txtProgressBar(min = 1, max = length(TextFiles), style = 3)
 TextList<-list()
 for(t in 1:length(TextFiles)){
   name<-TextFiles[t]
   name<-gsub(".txt","",name)
   x<-read.table(TextFiles[t])
   x<-as.matrix(x)
-  TextList[[t]]<-dropNA(x)
+  a<-dropNA(x)
+  b<-name
+  
+  TextList[[t]]<-list(b,a)
+  
+  # update progress bar
+  setTxtProgressBar(pb, t)
+    
 }
 
 fullnames<-names(TextList)
